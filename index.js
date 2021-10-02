12472965ff4eacbdff3143125f270ead10547de3
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2021-10-02 16:14:40 +0000

    questions

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 var readLineSync=require('readline-sync')
 var userName=readLineSync.question("What's your name?")
-console.log(" welcome " +userName+ " to Do You Know Rishabh?")
+console.log(" welcome " +userName+ " To The Ultimate Phineas And Ferb Quiz?")
 var score=0
 
 function play(question,answer){
 var userAnswer=readLineSync.question(question)
-if(userAnswer===answer)
+if(userAnswer.toLowerCase()===answer.toLowerCase())
 {
+  console.log("right answer")
 score++
 }
-else{}
+else{
+  console.log("wrong answer")
+}
+console.log("current score:",score)
+console.log("**************")
 }
 var questions=[{
-  question:"where do u live",
-  answer:"bangalore"
-},{question:"which college  do u study",
-  answer:"bangalore"},
-  {question:"where do u live",
-  answer:"bangalore"}
+  question:"What relation is Phineas to Ferb?",
+  answer:"step-brother"
+},{question:"What is Phineas' surname?",
+  answer:"flynn"},
+  {question:"What species is Perry?",
+  answer:"platypus"},
+  {question:"Who is agent P",
+  answer:"perry"},
+  {question:"What's their sister called?",
+  answer:"candace"}
 ]
 for(var i=0;i<questions.length;i++){
   var currentQuestion=questions[i]
